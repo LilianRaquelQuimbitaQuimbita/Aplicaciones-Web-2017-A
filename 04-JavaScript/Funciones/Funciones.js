cd25b5d96d2d5f51fbe88c84b54f6988e91e450f
LilianRaquelQuimbitaQuimbita <lilianraquelq@gmail.com>
2017-05-22 20:01:17 -0500

    1 parte JavaScript

---
+++ 04-JavaScript/Funciones/Funciones.js
@@ -0,0 +1,8 @@
+//1 Funcion
+function imprimirHola() {
+    console.log("Imprimir Hola Mundo")
+
+}
+imprimirHola();
+
+//
---
+++ 04-JavaScript/Variables.js
@@ -0,0 +1,45 @@
+
+//VARIABLES
+
+var texto = "";
+var texto2= '';
+var numero=1;
+var objeto={};
+var booleano = true;
+
+// ARREGLOS
+
+var arregloNumeros =[1,2,3,4,5];
+var arregloString = ["a","b","c","d"];
+var arregloBooleanos =[true,false, true];
+
+arregloNumeros = [1,"a",true,[1,"b"],{}];
+var matriz = [[1,2],[3,4]];
+//  0 1
+//0 1 2
+//1 2 3
+
+//OBJETOS
+
+var objetoNotacion ={};
+var objeto2 =
+    {
+        llave: "valor",
+        llave2: "valor",
+        llave3: "valor"
+    }
+objeto2.llave;
+objeto2.llave2;
+objeto2.llave3;
+
+var objetoCompleto =
+    {
+        nombre: "Lilian",
+        apellido: "Quimbita",
+        fechaNacimiento: new Date(),
+        mayorEdad:true,
+        peso:50,
+        altura:1.60,
+    }
+    console.log(objetoCompleto);
+
