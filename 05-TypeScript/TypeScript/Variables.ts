a9774e8cc01c4ca7f861024901184ded9f57e835
LilianRaquelQuimbitaQuimbita <lilianraquelq@gmail.com>
2017-06-03 08:04:36 -0500

    Uni todo a la rama master

---
+++ 05-TypeScript/TypeScript/Variables.ts
@@ -0,0 +1,19 @@
+//Arreglos
+
+let arregloNumeros:number[] =[1,2];
+let arregloNumeros2:Array <number>=[1,2];
+//INTERFACES
+//es como crear algo que necesitemos implementar
+interface  usuarioInt{
+    nombre:string,
+    apellido:string,
+    nota:number,
+    id:number
+}
+
+let cosa: usuarioInt={
+    nombre:"Lilian",
+    apellido:"Quimbita",
+    nota:20,
+    id:15,
+};
